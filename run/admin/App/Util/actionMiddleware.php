0156db413f2b47f3a8a435cd7842006777b571e7
jimmy <>
2017-12-04 16:13:59 +0800

    添加新方法!!!!!

--- run/admin/App/Util/actionMiddleware.php
+++ run/admin/App/Util/actionMiddleware.php
@@ -5,11 +5,10 @@
  * @version      	V1.0
  * @time        	17-06
  */
+class actionMiddleware extends Action {
 
-class actionMiddleware extends Action
-{
-    public $result = null;//错误信息
-    public $login_user = array();//用户登录信息
+    public $result = null; //错误信息
+    public $login_user = array(); //用户登录信息
 
     /**
      * 控制器所依赖模型 -- 定义后台所有使用到的Model引用
@@ -17,30 +16,31 @@ class actionMiddleware extends Action
      */
     public $models = array();
 
-    public function __construct(){
+    public function __construct() {
         $this->models = actionModels();
         SystemParams::set('OB', $this);
     }
+
     /**
      * 获取当前操作Model
      */
-    protected  final function getModel(){
-        $default = 'index' ;
-        if(defined('IsRewrite') && IsRewrite)	//是否开启地址重写
-        {
-            $url  = explode('/',$_SERVER['REQUEST_URI']);
-            $ext = substr($url[1],0,1);
-            if(empty($url[0]) && $ext == '?'){
+    protected final function getModel() {
+        $default = 'index';
+        if (defined('IsRewrite') && IsRewrite) { //是否开启地址重写
+            $url = explode('/', $_SERVER['REQUEST_URI']);
+            $ext = substr($url[1], 0, 1);
+            if (empty($url[0]) && $ext == '?') {
                 $mod = isset($_REQUEST['mod']) ? $_REQUEST['mod'] : $default;
-            }else{
+            } else {
                 $temp = array();
-                foreach($url as $k=>$v) if($v !=null) $temp[] = strtolower(trim($v));
-                $key  = Root == '/' ? 0 : count(explode('/',Root)) - 2;
-                $mod  = isset($temp[$key]) ? $temp[$key] : $default;
+                foreach ($url as $k => $v)
+                    if ($v != null)
+                        $temp[] = strtolower(trim($v));
+                $key = Root == '/' ? 0 : count(explode('/', Root)) - 2;
+                $mod = isset($temp[$key]) ? $temp[$key] : $default;
             }
         }
-        else
-        {
+        else {
             $mod = isset($_REQUEST['mod']) ? $_REQUEST['mod'] : $default;
         }
         return $mod;
@@ -50,19 +50,19 @@ class actionMiddleware extends Action
      * 获取当前操作Action
      * @access public
      */
-    protected  final function getAction(){
-        $default = 'index' ;
-        if(defined('IsRewrite') && IsRewrite)
-        {
-            $url  = explode('/',$_SERVER['REQUEST_URI']);
+    protected final function getAction() {
+        $default = 'index';
+        if (defined('IsRewrite') && IsRewrite) {
+            $url = explode('/', $_SERVER['REQUEST_URI']);
             $temp = array();
-            foreach($url as $k=>$v) if($v !=null) $temp[] = strtolower(trim($v));
-            $key  = Root == '/' ? 0 : count(explode('/',Root)) - 2;
-            $action  = isset($temp[$key+1]) ? $temp[$key+1] : $default;
+            foreach ($url as $k => $v)
+                if ($v != null)
+                    $temp[] = strtolower(trim($v));
+            $key = Root == '/' ? 0 : count(explode('/', Root)) - 2;
+            $action = isset($temp[$key + 1]) ? $temp[$key + 1] : $default;
         }
-        else
-        {
-            $action = isset($_REQUEST['action']) ? $_REQUEST['action'] : $default ;
+        else {
+            $action = isset($_REQUEST['action']) ? $_REQUEST['action'] : $default;
         }
         return $action;
     }
@@ -71,52 +71,69 @@ class actionMiddleware extends Action
      * 初始化相关参数，预先检查，用于公共检测，在继承中不能重写，在加载action之前做得工作可以放到这里
      * @access public
      */
-    protected final function before()
-    {
+    protected final function before() {
 
         extract($this->input);
 
-        $mod     = getModule();
-        $action  = getAction();
+        $mod = getModule();
+        $action = getAction();
 
         //获取用户登录信息
         if (loginCheck()) {
             $this->login_user = getAuth('all');
-
-        }elseif(!isset($this->nologin[$mod])||(isset($this->nologin[$mod])&&!in_array($action,$this->nologin[$mod]))){//登录操作
-	    
+        } elseif (!isset($this->nologin[$mod]) || (isset($this->nologin[$mod]) && !in_array($action, $this->nologin[$mod]))) {//登录操作
         }
     }
 
-   /**
-    * 重载 Object 里面的 import
-    * @param mixed $name
-    */  
-    function import($name =null) {
+    /**
+     * 重载 Object 里面的 import
+     * @param mixed $name
+     */
+    function import($name = null) {
         static $DBS = array();
-        $ob =  parent::import($name);//执行  Object   import 
-        
+        $ob = parent::import($name); //执行  Object   import 
         //如果返回空，说明 没有框架里面的model映射和 model文件 
-        if($ob == null){ 
-            if(isset($DBS[$name])){ 
-                return  $DBS[$name] ;   
-            }else{
-                $dbMapping = $this->com('dt')->tbl ;
-                if(isset($dbMapping[$name])){ 
-                   //$tableKey  $pK     $cached $expire
-                    $DB =  new modelMiddleware();   
-                    $DB->tableKey = $name;    
+        if ($ob == null) {
+            if (isset($DBS[$name])) {
+                return $DBS[$name];
+            } else {
+                $dbMapping = $this->com('dt')->tbl;
+                if (isset($dbMapping[$name])) {
+                    //$tableKey  $pK     $cached $expire
+                    $DB = new modelMiddleware();
+                    $DB->tableKey = $name;
                     $DB->com = $this->com;
                     $DB->mf = $this;
-                    $DB->input = $this->input;                   
-                    $DBS[$name] = $DB ; 
+                    $DB->input = $this->input;
+                    $DBS[$name] = $DB;
                     return $DB;
-                } 
+                }
             }
-            return  null ;
-        } 
-        return $ob  ;
+            return null;
+        }
+        return $ob;
     }
+
+    /**
+     * @param number $err
+     * @param unknown $msg
+     * @param string $url
+     * @param string $data
+     */
+    protected function praseJson($err = 0, $msg, $url = '', $data = '') {
+        if (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && $_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest') {
+            $msg = array('err' => $err, 'msg' => $msg, 'url' => $url, 'data' => $data);
+            die(json_encode($msg));
+        } else {
+            if ($url) {
+                header('Location:' . $url);
+                exit;
+            }
+            header('Location:' . Root);
+            exit;
+        }
+    }
+
 }
 
 ?>
