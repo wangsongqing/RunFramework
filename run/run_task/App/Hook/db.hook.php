3c89680ec5dd5cbf7b77c4d12c33d79aaadfb99b
jimmy <>
2018-02-22 16:04:50 +0800

    cmd 执行 优化

--- run/run_task/App/Hook/db.hook.php
+++ run/run_task/App/Hook/db.hook.php
@@ -1,55 +1,55 @@
 <?php
+class DbHook
+{
+	/**
+	 * SQL语句
+	 */
+	public  $sql    = null;
 
-class DbHook {
+	/**
+	 * SQL日志
+	 */
+	public  $sqlLog = null;
 
-    /**
-     * SQL语句
-     */
-    public $sql = null;
+	/**
+	 * 开始执行SQL的时间
+	 */
+	private $startTime  = null;
 
-    /**
-     * SQL日志
-     */
-    public $sqlLog = null;
+	/**
+	 * 结束时间
+	 */
+	private $endTime    = null;
 
-    /**
-     * 开始执行SQL的时间
-     */
-    private $startTime = null;
+	/**
+	 * SQL计数器
+	 */
+	private $loop  = 0;
 
-    /**
-     * 结束时间
-     */
-    private $endTime = null;
-
-    /**
-     * SQL计数器
-     */
-    private $loop = 0;
-
-    /**
-     * 记录SQL日志
-     *
-     * @access public
-     * @return void
-     */
-    public function work() {
-        ++$this->loop;
+	/**
+	 * 记录SQL日志
+	 *
+	 * @access public
+	 * @return void
+	 */
+	public function work()
+	{
+		++$this->loop;
 // 		$this->sqlLog .= "<fieldset><legend><font color=red>第 {$this->loop} 次查询</font></legend>{$this->sql}</fieldset><br>";
 // 		$this->endTime = array_sum(split(' ', microtime()));
 // 		writeLog($this->sql, 'sql.log');
-    }
+	}
 
-    /**
-     * 获取执行SQL的时间
-     *
-     * @access public
-     * @return void
-     */
-    public function getTime() {
-        echo '<div align=center>Process: ' . number_format((array_sum(split(' ', microtime())) - $this->endTime), 6) . 's</div>';
-    }
 
+	/**
+	 * 获取执行SQL的时间
+	 *
+	 * @access public
+	 * @return void
+	 */
+	public function getTime()
+	{
+		//echo '<div align=center>Process: '.number_format((array_sum(split(' ', microtime())) - $this->endTime), 6).'s</div>';
+	}
 }
-
 ?>
