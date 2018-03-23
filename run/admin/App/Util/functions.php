497e427d79344cd2193b6ce4c20fa59494b0d345
jimmy <>
2018-03-23 13:18:11 +0800

    共用model

--- run/admin/App/Util/functions.php
+++ run/admin/App/Util/functions.php
@@ -1,7 +1,8 @@
 <?php
 //函数列表
-require_once('include/function.cookie.php');
-require_once('include/function.app.php');
-require_once('include/function.sesame.php');
-require_once('include/function.redis.php');
+require_once('../api/include/function.cookie.php');//cookie全局函数
+require_once('../api/include/function.redis.php');//redis全局函数
+require_once('../api/include/function.cache.php');//cache全局函数
+require_once('../api/include/function.global.php');//全局函数
+
 ?>
