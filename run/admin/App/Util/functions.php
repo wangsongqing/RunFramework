41c2dbd90b120ed58192645fdda609a10fd339d1
jimmy <>
2018-04-13 14:43:24 +0800

    api类的调用优化。。。。。

--- run/admin/App/Util/functions.php
+++ run/admin/App/Util/functions.php
@@ -4,5 +4,5 @@ require_once('../api/include/function.cookie.php');//cookie全局函数
 require_once('../api/include/function.redis.php');//redis全局函数
 require_once('../api/include/function.cache.php');//cache全局函数
 require_once('../api/include/function.global.php');//全局函数
-
+require_once('include/function.auth.php');//全局函数
 ?>
